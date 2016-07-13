f475a85fd22ab35b9248cbc253b685c28f21b0b1
SamLaio <f85122@gmail.com>
2016-07-13 16:25:28 +0800

    V 0.5

--- Base64ImgReSize.php
+++ Base64ImgReSize.php
@@ -28,7 +28,7 @@ class Base64ImgReSize{
 	private $Config;
 	private $Filename;
 	private $Path;
-	private $Type;
+	private $Type = '';
 	public function Load($file, $width, $height, $path = false, $filename = false) {
 		if(is_file($file)){
 			$imgData = base64_encode(file_get_contents($file));
@@ -43,80 +43,99 @@ class Base64ImgReSize{
 			$this->Type = 'jpeg';
 			$this->Img = str_replace('data:image/jpeg;base64,','', $this->Img);
 		}
-		$this->Img = str_replace(' ', '+', $this->Img);
-		$Tmp = base64_decode($this->Img);
-		$this->Img = imagecreatefromstring($Tmp);
-		$this->ImgInfo = getimagesizefromstring($Tmp);
-		$this->Config = array('width'=>$width,'height'=>$height);
-		$this->Filename = ($filename)?$filename:'';
-		$this->Path = '';
-		if($path){
-			$tmp = '';
-			foreach(explode('/',$path) as $val){
-				if($val != ''){
-					if($tmp != ''){
-						$tmp .= '/';
+		if($this->Type != ''){
+			$this->Img = str_replace(' ', '+', $this->Img);
+			$Tmp = base64_decode($this->Img);
+			$this->Img = imagecreatefromstring($Tmp);
+			$this->ImgInfo = getimagesizefromstring($Tmp);
+			$this->Config = array('width'=>$width,'height'=>$height);
+			$this->Filename = ($filename)?$filename:'';
+			$this->Path = '';
+			if($path){
+				$tmp = '';
+				foreach(explode('/',$path) as $val){
+					if($val != ''){
+						if($tmp != ''){
+							$tmp .= '/';
+						}
+						$tmp .= $val;
 					}
-					$tmp .= $val;
 				}
+				$this->Path = $tmp . '/';
 			}
-			$this->Path = $tmp . '/';
+		}else{
+			echo "Input Img Error.";
 		}
 	}
 	public function ReSize(){
-		$width = $this->ImgInfo[0];
-		$height = $this->ImgInfo[1];
-		if(
-			$this->ImgInfo[0] > $this->Config['width'] or
-			$this->ImgInfo[1] > $this->Config['height']
-		){
-			if($this->ImgInfo[0] > $this->ImgInfo[1]){
-				if($this->ImgInfo[0] > $this->Config['width']){
-					$ratio = $this->Config['width'] / $this->ImgInfo[0];
-					$ratio = round($ratio, 3);
-					$height = ceil($this->ImgInfo[1] * $ratio);
-					$width = $this->Config['width'];
-				}
-			}else{
-				if($this->ImgInfo[1] > $this->Config['height']){
-					$ratio = $this->Config['height'] / $this->ImgInfo[1];
-					$ratio = round($ratio, 3);
-					$width = ceil($this->ImgInfo[0] * $ratio);
-					$height = $this->Config['height'];
+		if($this->Type != ''){
+			$width = $this->ImgInfo[0];
+			$height = $this->ImgInfo[1];
+			if(
+				$this->ImgInfo[0] > $this->Config['width'] or
+				$this->ImgInfo[1] > $this->Config['height']
+			){
+				if($this->ImgInfo[0] > $this->ImgInfo[1]){
+					if($this->ImgInfo[0] > $this->Config['width']){
+						$ratio = $this->Config['width'] / $this->ImgInfo[0];
+						$ratio = round($ratio, 3);
+						$height = ceil($this->ImgInfo[1] * $ratio);
+						$width = $this->Config['width'];
+					}
+				}else{
+					if($this->ImgInfo[1] > $this->Config['height']){
+						$ratio = $this->Config['height'] / $this->ImgInfo[1];
+						$ratio = round($ratio, 3);
+						$width = ceil($this->ImgInfo[0] * $ratio);
+						$height = $this->Config['height'];
+					}
 				}
 			}
+			$new_image = imagecreatetruecolor($width, $height);
+			imagecopyresampled($new_image, $this->Img, 0, 0, 0, 0, $width, $height, $this->ImgInfo[0], $this->ImgInfo[1]);
+			$this->Img = $new_image;
+		}else{
+			echo "Input Img Error.";
+			return false;
 		}
-		$new_image = imagecreatetruecolor($width, $height);
-		imagecopyresampled($new_image, $this->Img, 0, 0, 0, 0, $width, $height, $this->ImgInfo[0], $this->ImgInfo[1]);
-		$this->Img = $new_image;
 	}
 	public function Save() {
-		$this->Filename = ($this->Filename == '')?md5(date('YmdHis').rand(100,1000)):$this->Filename;
-		$this->Filename = $this->Path.$this->Filename;
-		if($this->Type == 'jpeg'){
-			$this->Filename = $this->Filename.'.jpg';
-			imagejpeg($this->Img,$this->Filename,75);
+		if($this->Type != ''){
+			$this->Filename = ($this->Filename == '')?md5(date('YmdHis').rand(100,1000)):$this->Filename;
+			$this->Filename = $this->Path.$this->Filename;
+			if($this->Type == 'jpeg'){
+				$this->Filename = $this->Filename.'.jpg';
+				imagejpeg($this->Img,$this->Filename,75);
+			}
+			if($this->Type == 'png'){
+				$this->Filename = $this->Filename.'.png';
+				imagepng($this->Img,$this->Filename);
+			}
+			//修改權限
+			chmod($this->Filename,0755);
+			return $this->Filename;
+		}else{
+			echo "Input Img Error.";
+			return false;
 		}
-		if($this->Type == 'png'){
-			$this->Filename = $this->Filename.'.png';
-			imagepng($this->Img,$this->Filename);
-		}
-		//修改權限
-		chmod($this->Filename,0755);
-		return $this->Filename;
 	}
 	public function GetImg(){
-		$ret = "data:image/".$this->Type.";base64,";
-		ob_start ();
-		if($this->Type == 'jpeg') {
-			imagejpeg ($this->Img);
+		if($this->Type != ''){
+			$ret = "data:image/".$this->Type.";base64,";
+			ob_start();
+			if($this->Type == 'jpeg') {
+				imagejpeg ($this->Img);
+			}
+			if($this->Type == 'png') {
+				imagepng ($this->Img);
+			}
+			$this->Img = ob_get_contents();
+			ob_end_clean();
+			$ret .= base64_encode($this->Img);
+			return $ret;
+		}else{
+			echo "Input Img Error.";
+			return false;
 		}
-		if($this->Type == 'png') {
-			imagepng ($this->Img);
-		}
-		$this->Img = ob_get_contents();
-		ob_end_clean ();
-		$ret .= base64_encode($this->Img);
-		return $ret;
 	}
 }
